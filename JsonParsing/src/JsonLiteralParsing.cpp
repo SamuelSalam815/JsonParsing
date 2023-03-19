d59b0aedab47e149c8214c45854f1d7d627c740b
sam <samuelsalam815@gmail.com>
2023-03-19 18:02:47 +0000

    Implemented string parsing with control sequences. Compacted long switch-case statements.

--- JsonParsing/src/JsonLiteralParsing.cpp
+++ JsonParsing/src/JsonLiteralParsing.cpp
@@ -1,6 +1,21 @@
 #include "..\include\JsonLiteralParsing.h"
 #include <vector>
 
+std::string EOF_Error_Message = "Unexpected end of input!";
+template<class T>
+bool vector_contains(std::vector<T> searchSpace, T target)
+{
+	std::vector<T>::iterator iterator;
+	for (iterator = searchSpace.begin(); iterator != searchSpace.end(); iterator++)
+	{
+		if (*iterator == target)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
 JsonValueType NextExpectedValueType(char c)
 {
 	switch (c)
@@ -56,79 +71,99 @@ void AssertStringIsNext(ParsingInputPtr context, std::string expectedString)
 double ParseNumber(ParsingInputPtr context)
 {
 	SkipWhiteSpace(context);
-	switch (context->peek())
+	
+	std::vector<char> validFirstCharacters = { '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+	if (!vector_contains(validFirstCharacters, context->peek()))
 	{
-	case '-':
-	case '0':
-	case '1':
-	case '2':
-	case '3':
-	case '4':
-	case '5':
-	case '6':
-	case '7':
-	case '8':
-	case '9':
-		break;
-	default:
 		throw JsonParsingException("Expected number", context);
-		break;
 	}
 
 	std::vector<char> charsRead = std::vector<char>();
+	std::vector<char> allowedCharacters = { '-', '+', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'E', 'e', '.' };
 	bool nextCharIsValid = true;
 	while (context->good() && nextCharIsValid)
 	{
-		switch (context->peek())
+		if (vector_contains(allowedCharacters, context->peek()))
 		{
-		case '-':
-		case '+':
-		case '0':
-		case '1':
-		case '2':
-		case '3':
-		case '4':
-		case '5':
-		case '6':
-		case '7':
-		case '8':
-		case '9':
-		case 'E':
-		case 'e':
-		case '.':
 			charsRead.push_back(context->get());
-			break;
-		default:
-			goto FinishedGettingChars;
+		}
+		else
+		{
+			char* ptrToFirstElement = &(charsRead[0]);
+			return std::atof(ptrToFirstElement);
 		}
 	}
-	FinishedGettingChars:
+}
 
-	char* ptrToFirstElement = &(charsRead[0]);
-	return std::atof(ptrToFirstElement);
+std::string GetUnicodeSequence(ParsingInputPtr context)
+{
+	std::string result = "";
+	std::vector<char> validHexDigits =
+	{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+	  'A', 'B', 'C', 'D', 'E', 'F',
+	  'a', 'b', 'c', 'd', 'e', 'f' };
+	for (int charactersParsed = 0; charactersParsed < 4; charactersParsed++)
+	{
+		if (!context->good())
+		{
+			throw JsonParsingException(EOF_Error_Message, context);
+		}
+		char nextCharacter = context->get();
+		if (vector_contains(validHexDigits, nextCharacter))
+		{
+			result += nextCharacter;
+		}
+		else
+		{
+			throw JsonParsingException("Expected Valid Hex digit!", context);
+		}
+	}
+}
+
+std::string GetExcapedSequence(ParsingInputPtr context)
+{
+	while (context->good())
+	{
+		char nextCharacter = context->get();
+		switch (nextCharacter)
+		{
+		case '\\':
+		case '/':
+		case 'b':
+		case 'f':
+		case 'n':
+		case 'r':
+		case 't':
+			return "" + nextCharacter;
+		case 'u':
+			return "u" + GetUnicodeSequence(context);
+		default:
+			throw JsonParsingException("Expected valid control sequence ('\\', '/', 'b', 'f', 'n', 'r', 't' or 'u' + 4 hex digits) !", context);
+		}
+	}
+	throw JsonParsingException(EOF_Error_Message, context);
 }
 
 std::string ParseString(ParsingInputPtr context)
 {
-	throw JsonParsingException("String parsing not supported yet!", context);
-	if(context.get() != '\"')
+	if(context->get() != '\"')
 	{
 		throw JsonParsingException("Expected \"!", context);
 	}
 	std::string result = "";
-	while(context.good())
+	while(context->good())
 	{
 		char currentCharacter;
-		switch(currentCharacter = context.get())
+		switch(currentCharacter = context->get())
 		{
 			case '\\':
-				throw JsonParsingException("Control characters not yet supported!", context);
+				result += "\\" + GetExcapedSequence(context);
 				break;
-			case "\"":
+			case '\"':
 				return result;
 			default:
 				result += currentCharacter;
 		}
 	}
-	throw JsonParsingException("Unexpected end of input!", context);
+	throw JsonParsingException(EOF_Error_Message, context);
 }
